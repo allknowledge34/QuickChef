19a2692e2696c7d351a0a4c488aede5fe42c7ced
SachinKumar <166914290+allknowledge34@users.noreply.github.com>
2025-02-05 22:40:22 +0530

    Update doctorController.js

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 import doctorModel from "../models/doctorModel.js"
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 import appointmentModel from "../models/appointmentModel.js"
 
