2746439cfc5ca8cc0d0557537d1844a528ea80e4
sachin kumar <allknowledge34@gmail.com>
2025-02-05 21:43:44 +0530

    first change

---
+++ admin/src/pages/Admin/AddDoctor.jsx
@@ -0,0 +1,208 @@
+// Copyright 2025 PREM
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     https://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import React, { useContext, useState } from 'react'
+import { assets } from '../../assets/assets'
+import { toast } from 'react-toastify'
+import axios from 'axios'
+import { AdminContext } from '../../context/AdminContext'
+import { AppContext } from '../../context/AppContext'
+
+const AddDoctor = () => {
+
+    const [docImg, setDocImg] = useState(false)
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [experience, setExperience] = useState('1 Year')
+    const [fees, setFees] = useState('')
+    const [about, setAbout] = useState('')
+    const [speciality, setSpeciality] = useState('Chef')
+    const [degree, setDegree] = useState('')
+    const [address1, setAddress1] = useState('')
+    const [address2, setAddress2] = useState('')
+
+    const { backendUrl } = useContext(AppContext)
+    const { aToken } = useContext(AdminContext)
+
+    const onSubmitHandler = async (event) => {
+        event.preventDefault()
+
+        try {
+
+            if (!docImg) {
+                return toast.error('Image Not Selected')
+            }
+
+            const formData = new FormData();
+
+            formData.append('image', docImg)
+            formData.append('name', name)
+            formData.append('email', email)
+            formData.append('password', password)
+            formData.append('experience', experience)
+            formData.append('fees', Number(fees))
+            formData.append('about', about)
+            formData.append('speciality', speciality)
+            formData.append('degree', degree)
+            formData.append('address', JSON.stringify({ line1: address1, line2: address2 }))
+
+            // console log formdata            
+            formData.forEach((value, key) => {
+                console.log(`${key}: ${value}`);
+            });
+
+            const { data } = await axios.post(backendUrl + '/api/admin/add-doctor', formData, { headers: { aToken } })
+            if (data.success) {
+                toast.success(data.message)
+                setDocImg(false)
+                setName('')
+                setPassword('')
+                setEmail('')
+                setAddress1('')
+                setAddress2('')
+                setDegree('')
+                setAbout('')
+                setFees('')
+            } else {
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            toast.error(error.message)
+            console.log(error)
+        }
+
+    }
+
+    return (
+        <form onSubmit={onSubmitHandler} className='m-5 w-full'>
+
+            <p className='mb-3 text-lg font-medium'>Add Chef</p>
+
+            <div className='bg-white px-8 py-8 border rounded w-full max-w-4xl max-h-[80vh] overflow-y-scroll'>
+                <div className='flex items-center gap-4 mb-8 text-gray-500'>
+                    <label htmlFor="doc-img">
+                        <img className='w-16 bg-gray-100 rounded-full cursor-pointer' src={docImg ? URL.createObjectURL(docImg) : assets.upload_area} alt="" />
+                    </label>
+                    <input onChange={(e) => setDocImg(e.target.files[0])} type="file" name="" id="doc-img" hidden />
+                    <p>Upload Chef <br /> picture</p>
+                </div>
+
+                <div className='flex flex-col lg:flex-row items-start gap-10 text-gray-600'>
+
+                    <div className='w-full lg:flex-1 flex flex-col gap-4'>
+
+                        <div className='flex-1 flex flex-col gap-1'>
+                            <p>Your name</p>
+                            <input onChange={e => setName(e.target.value)} value={name} className='border rounded px-3 py-2' type="text" placeholder='Name' required />
+                        </div>
+
+                        <div className='flex-1 flex flex-col gap-1'>
+                            <p>Chef Email</p>
+                            <input onChange={e => setEmail(e.target.value)} value={email} className='border rounded px-3 py-2' type="email" placeholder='Email' required />
+                        </div>
+
+
+                        <div className='flex-1 flex flex-col gap-1'>
+                            <p>Set Password</p>
+                            <input onChange={e => setPassword(e.target.value)} value={password} className='border rounded px-3 py-2' type="password" placeholder='Password' required />
+                        </div>
+
+                        <div className='flex-1 flex flex-col gap-1'>
+                            <p>Experience</p>
+                            <select onChange={e => setExperience(e.target.value)} value={experience} className='border rounded px-2 py-2' >
+                                <option value="1 Year">1 Year</option>
+                                <option value="2 Year">2 Years</option>
+                                <option value="3 Year">3 Years</option>
+                                <option value="4 Year">4 Years</option>
+                                <option value="5 Year">5 Years</option>
+                                <option value="6 Year">6 Years</option>
+                                <option value="8 Year">8 Years</option>
+                                <option value="9 Year">9 Years</option>
+                                <option value="11 Year">10 Years</option>
+                                <option value="12 Year">11 Years</option>
+                                <option value="13 Year">12 Years</option>
+                                <option value="14 Year">13 Years</option>
+                                <option value="15 Year">14 Years</option>
+                                <option value="16 Year">15 Years</option>
+                                <option value="17 Year">16 Years</option>
+                                <option value="18 Year">17 Years</option>
+                                <option value="19 Year">18 Years</option>
+                                <option value="20 Year">19 Years</option>
+                                <option value="21 Year">20 Years</option>
+                                <option value="22 Year">22 Years</option>
+                                <option value="23 Year">23 Years</option>
+                                <option value="24 Year">24 Years</option>
+                                <option value="25 Year">25 Years</option>
+                                <option value="26 Year">26 Years</option>
+                                <option value="27 Year">27 Years</option>
+                                <option value="28 Year">28 Years</option>
+                                <option value="29 Year">29 Years</option>
+                                <option value="30 Year">30 Years</option>
+                                <option value="30 Year +">30 Years +</option>
+                            </select>
+                        </div>
+
+                        <div className='flex-1 flex flex-col gap-1'>
+                            <p>Fees</p>
+                            <input onChange={e => setFees(e.target.value)} value={fees} className='border rounded px-3 py-2' type="number" placeholder='Chef fees' required />
+                        </div>
+
+                    </div>
+
+                    <div className='w-full lg:flex-1 flex flex-col gap-4'>
+
+                        <div className='flex-1 flex flex-col gap-1'>
+                            <p>Speciality</p>
+                            <select onChange={e => setSpeciality(e.target.value)} value={speciality} className='border rounded px-2 py-2'>
+                                <option value="Chef">Chef</option>
+                                <option value="Classes">Classes</option>
+                                <option value="Testing">Testing</option>
+                                <option value="Nutrition">Nutrition</option>
+                                <option value="Recipes">Recipes</option>
+                            </select>
+                        </div>
+
+
+                        <div className='flex-1 flex flex-col gap-1'>
+                            <p>Degree</p>
+                            <input onChange={e => setDegree(e.target.value)} value={degree} className='border rounded px-3 py-2' type="text" placeholder='Degree' required />
+                        </div>
+
+                        <div className='flex-1 flex flex-col gap-1'>
+                            <p>Address</p>
+                            <input onChange={e => setAddress1(e.target.value)} value={address1} className='border rounded px-3 py-2' type="text" placeholder='Address 1' required />
+                            <input onChange={e => setAddress2(e.target.value)} value={address2} className='border rounded px-3 py-2' type="text" placeholder='Address 2' required />
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div>
+                    <p className='mt-4 mb-2'>About Chef</p>
+                    <textarea onChange={e => setAbout(e.target.value)} value={about} className='w-full px-4 pt-2 border rounded' rows={5} placeholder='write about chef'></textarea>
+                </div>
+
+                <button type='submit' className='bg-primary px-10 py-3 mt-4 text-white rounded-full'>Add Chef</button>
+
+            </div>
+
+
+        </form>
+    )
+}
+
+export default AddDoctor
